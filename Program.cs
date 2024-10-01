4b5c289d502d3c24911f6568450e9de5ec71deb8
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2024-10-01 02:38:34 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bool {
+    class Program {
+        static void Main(string[] args) {
+            bool value = true;
+            bool isMale = true;
+        
+
+            Console.WriteLine(isMale);
+            
+
+            isMale = false;
+
+            Console.WriteLine(isMale);
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+}
+
